a4c12bd5660c7ca5aac9552287f9849c6fc2d516
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2013-12-13 12:07:29 +0100

    little fix in the lazy lists unit test

--- cram_utilities/tests/lazy-lists.lisp
+++ cram_utilities/tests/lazy-lists.lisp
@@ -134,13 +134,13 @@
     (assert-equal '(:elem :elem :elem) list)))
 
 (define-test lazy-dynamic-environment
-  (let ((*foo* nil))
-    (declare (special *foo))
+  (let ((*foo* :initial))
+    (declare (special *foo*))
     (let ((lazy-list
               (with-lazy-list-dynamic-environment
-                  ((*foo* :bar))
+                  ((*foo* :new))
                 (lazy-list ((i 2))
                   (when (> i 0)
                     (cont *foo* (1- i)))))))
-      (assert-eq (lazy-elt lazy-list 0) :bar)
-      (assert-eq (lazy-elt lazy-list 1) :bar))))
+      (assert-eq :initial (lazy-elt lazy-list 0))
+      (assert-eq :initial (lazy-elt lazy-list 1)))))
